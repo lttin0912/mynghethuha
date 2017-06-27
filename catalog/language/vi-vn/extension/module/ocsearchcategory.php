f0c6679742359ae9c4f04cc58b77324b2b0f7a98
Tin Ly <lttin0912@gmail.com>
2017-06-27 13:08:37 +0700

    fix text

--- catalog/language/vi-vn/extension/module/ocsearchcategory.php
+++ catalog/language/vi-vn/extension/module/ocsearchcategory.php
@@ -1,6 +1,6 @@
 <?php
 // Text
-$_['text_search'] = 'Search';
-$_['text_category']     = 'All Categories';
-$_['text_empty']        = 'There is no product that matches the search criteria.';
-$_['text_tax']        = 'Ex Tax:';
+$_['text_search']   = 'Tìm kiếm';
+$_['text_category'] = 'Tất cả danh mục';
+$_['text_empty']    = 'There is no product that matches the search criteria.';
+$_['text_tax']      = 'Ex Tax:';
