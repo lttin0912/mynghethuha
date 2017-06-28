427cff2b9f169208c0c025e1f8b8562f051adda4
Tin Ly <lttin0912@gmail.com>
2017-06-28 21:25:37 +0700

    update

--- catalog/language/vi-vn/common/footer.php
+++ catalog/language/vi-vn/common/footer.php
@@ -14,4 +14,7 @@ $_['text_account']      = 'Tài khoản của tôi';
 $_['text_order']        = 'Lịch sử đơn hàng';
 $_['text_wishlist']     = 'Danh sách yêu thích';
 $_['text_newsletter']   = 'Thư thông báo';
-$_['text_powered']      = 'Việt hóa bởi <a href="http://vietxuagas.com.vn">VietXuanGas</a><br /> %s &copy; %s';
+$_['text_powered']      = 'Website made by Bii<br /> %s &copy; %s';
+$_['text_address']      = 'Địa chỉ:';
+$_['text_telephone']    = 'Điện thoại:';
+$_['f_desc']            = 'Mỹ Nghệ Thu Hà là DNTN chuyên sản xuất gia công các mặt hàng về gỗ quý thuộc nhóm I với giá cả phù hợp, đảm bảo mẫu mã và chất lượng.<br>Mỹ Nghệ Thu Hà hi vọng sẽ mang đến cho quý khách hàng sự hài lòng nhất.';
